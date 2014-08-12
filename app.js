68b80ca509af3e1fb6a5740c6b8cf539149d9e46
Sean Michael <seankmichael@gmail.com>
2014-08-12 16:25:35 -0600

    fix the catch all function in app.js to work in 'dist' folder

--- app.js
+++ app.js
@@ -17,9 +17,9 @@ var port = process.env.PORT || 8080;
 mongoose.connect(database.url);
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static( __dirname + '/dist/views'));
+app.use(express.static( __dirname + 'dist/views'));
 app.all('/*', function(req, res) {
-	res.sendfile('./dist/index.html', { root: 'dist' });
+	res.sendfile('./index.html', { root: 'dist' });
 });
 
 // --------------------------------------------------
